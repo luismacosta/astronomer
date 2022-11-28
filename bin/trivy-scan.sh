159bdd56e39ae95b9df4dd483f4414944d5830dc
Mishal Shah <mishal.shah@astronomer.io>
2022-11-28 17:12:34 +0530

    Uploading scan result to github code scan

--- bin/trivy-scan.sh
+++ bin/trivy-scan.sh
@@ -17,6 +17,7 @@ trivy \
   --ignore-unfixed \
   --exit-code 1 \
   --no-progress \
+  -f sarif \
   "${scan_target}" > "${GIT_ROOT}/trivy-output.txt"
 exit_code=$?
 
@@ -27,6 +28,14 @@ cat "${GIT_ROOT}/trivy-output.txt"
 if grep -q -i 'OS is not detected' trivy-output.txt ; then
   echo "Skipping trivy scan because of unsupported OS"
   exit 0
+else
+  sarif_base64=$(gzip -c "${GIT_ROOT}/trivy-output.txt" | base64)
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    https://api.github.com/repos/astronomer/astronomer/code-scanning/sarifs \
+    -d '{"commit_sha":"4b6472266afd7b471e86085a6659e8c7f2b119da","ref":"refs/heads/master","sarif":"${sarif_base64}"}'
 fi
 
 exit "${exit_code}"
