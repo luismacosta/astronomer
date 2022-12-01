dc1b623bcd42e4b0262e1326224de1968228194f
Mishal Shah <mishal.shah@astronomer.io>
2022-12-01 12:46:11 +0530

    Adding info.

--- bin/trivy-scan-publish.sh
+++ bin/trivy-scan-publish.sh
@@ -18,18 +18,25 @@ trivy_result_dir="$1"
 set +exo pipefail
 
 echo "Merging scan results to single file"
+
+# Sarif Tools Copy Ref: https://github.com/microsoft/sarif-tools#copy
 sarif copy --output "${GIT_ROOT}/final.sarif" "${GIT_ROOT}/${trivy_result_dir}"
 
-echo "Publishing the Trivy scan result to Github Security - Code Scanning"
 sarif_base64=$(gzip -c "${GIT_ROOT}/final.sarif" | base64 -w0)
+
+echo "Publishing the Trivy scan result to Github Security - Code Scanning"
+
+# Github Upload Sarif Ref: https://docs.github.com/en/rest/code-scanning?apiVersion=2022-11-28#upload-an-analysis-as-sarif-data
 curl -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   https://api.github.com/repos/astronomer/astronomer/code-scanning/sarifs \
   -d '{"commit_sha":'"\"${GIT_COMMIT_SHA}\""',"ref":'"\"refs/heads/${GIT_BRANCH}\""',"started_at":'"\"${TIMESTAMP}\""',"sarif":'"\"${sarif_base64}\""'}'
 
+# Sarif Tools Copy Ref: https://github.com/microsoft/sarif-tools#csv
 sarif csv --output "${GIT_ROOT}/astronomer_vul_${DATE_STR}.csv" "${GIT_ROOT}/final.sarif"
 
+# Upload File Ref: https://api.slack.com/methods/files.upload#arg_file
 curl -F file=@"${GIT_ROOT}/astronomer_vul_${DATE_STR}.csv" -F "initial_comment=Trivy Vulnerability Scan Report for repo ${GIT_REPO_NAME} of branch ${GIT_BRANCH}. " -F channels=C03HS1H9G1E -H "Authorization: Bearer $SLACK_ACCESS_TOKEN" https://slack.com/api/files.upload
 
 exit $?
