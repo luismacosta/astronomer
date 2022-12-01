da8d51f79fe6cf8d1abc5dc80712dd433708299b
Mishal Shah <mishal.shah@astronomer.io>
2022-12-01 11:58:18 +0530

    Updaring message.

--- bin/trivy-scan-publish.sh
+++ bin/trivy-scan-publish.sh
@@ -6,10 +6,12 @@
 }
 [ -f /etc/os-release ] && cat /etc/os-release
 
+GIT_REPO_NAME=$(basename -s .git "$(git config --get remote.origin.url)")
 GIT_ROOT="$(git -C "${0%/*}" rev-parse --show-toplevel)"
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GIT_COMMIT_SHA=$(git rev-parse HEAD)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+DATE_STR=$(date -u +"%Y-%m-%d")
 
 trivy_result_dir="$1"
 
@@ -27,8 +29,8 @@ curl -X POST \
   https://api.github.com/repos/astronomer/astronomer/code-scanning/sarifs \
   -d '{"commit_sha":'"\"${GIT_COMMIT_SHA}\""',"ref":'"\"refs/heads/${GIT_BRANCH}\""',"started_at":'"\"${TIMESTAMP}\""',"sarif":'"\"${sarif_base64}\""'}'
 
-sarif csv --output "${GIT_ROOT}/report.csv" "${GIT_ROOT}/final.sarif"
+sarif csv --output "${GIT_ROOT}/astronomer_vul_${DATE_STR}.csv" "${GIT_ROOT}/final.sarif"
 
-curl -F file=@"${GIT_ROOT}/final.sarif" -F "initial_comment=Example upload" -F channels=C03HS1H9G1E -H "Authorization: Bearer $SLACK_ACCESS_TOKEN" https://slack.com/api/files.upload
+curl -F file=@"${GIT_ROOT}/astronomer_vul_${DATE_STR}.csv" -F "initial_comment=Trivy Vulnerability Scan Report for repo ${GIT_REPO_NAME} of branch ${GIT_BRANCH}. " -F channels=C03HS1H9G1E -H "Authorization: Bearer $SLACK_ACCESS_TOKEN" https://slack.com/api/files.upload
 
 exit $?
